af5b933d044121eb30ae741b48eb55fe42ecc10b
shimizu <ayoaho@yahoo.co.jp>
2022-12-29 20:18:25 +0900

    グラフ化までできた

--- PLGetter.py
+++ PLGetter.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sqlite3.dbapi2 import Cursor
 from edinet_xbrl.edinet_xbrl_parser import EdinetXbrlParser
 import os
@@ -7,8 +8,32 @@ import datetime
 from datetime import date
 from itertools import product
 
+
 # DBの指定
 DB = 'D:/XBRLDB/XBRL_DB_v02.db'
 
 # sqlの指定
 sql = 'SELECT * FROM XbrlDB'
+
+# ダウンロード済のXbrlファイルのリストを作成
+conn = sqlite3.connect(DB)
+cur = conn.cursor()
+df = pd.read_sql(sql, conn)
+
+print(df.columns)
+
+# 銘柄コードで抽出(Announcement_dateでソート)
+df_select = df[df['Code'] == 6196].sort_values('Announcement_date')
+
+print(df_select['Announcement_date'])
+print(df_select['Sales'])
+
+xlist = df_select['Announcement_date'].values.tolist()
+# xlist = df_select['OperatingIncome'].values.tolist()
+ylist = df_select['Sales'].values.tolist()
+
+plt.scatter(xlist, ylist)
+plt.show()
+
+
+
